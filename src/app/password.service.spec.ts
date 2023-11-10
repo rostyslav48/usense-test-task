21b2183d0439b52fa6eaa3aa680c972b68645fa5
rostyslav48 <rostyslavkravchuk05@gmail.com>
2023-11-10 13:50:50 +0200

    add tests to passwordService

--- src/app/password.service.spec.ts
+++ src/app/password.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PasswordService } from './password.service';
+import { PasswordStrength } from '../shared/enums/password-strength.enum';
 
 describe('PasswordService', () => {
   let service: PasswordService;
@@ -12,5 +13,45 @@ describe('PasswordService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Should be default', () => {
+    const TESTED_PASSWORD = '';
+    const EXPECTED_VALUE = PasswordStrength.Default;
+    const result = service.checkPasswordStrength(TESTED_PASSWORD);
+
+    expect(result).toBe(EXPECTED_VALUE);
+  });
+
+  it('Should be short', () => {
+    const TESTED_PASSWORD = 'qwe1@';
+    const EXPECTED_VALUE = PasswordStrength.Short;
+    const result = service.checkPasswordStrength(TESTED_PASSWORD);
+
+    expect(result).toBe(EXPECTED_VALUE);
+  });
+
+  it('Should be weak', () => {
+    const TESTED_PASSWORD = '12345678';
+    const EXPECTED_VALUE = PasswordStrength.Weak;
+    const result = service.checkPasswordStrength(TESTED_PASSWORD);
+
+    expect(result).toBe(EXPECTED_VALUE);
+  });
+
+  it('Should be medium', () => {
+    const TESTED_PASSWORD = '12345678av';
+    const EXPECTED_VALUE = PasswordStrength.Medium;
+    const result = service.checkPasswordStrength(TESTED_PASSWORD);
+
+    expect(result).toBe(EXPECTED_VALUE);
+  });
+
+  it('Should be strong', () => {
+    const TESTED_PASSWORD = '12345678qw#4!';
+    const EXPECTED_VALUE = PasswordStrength.Strong;
+    const result = service.checkPasswordStrength(TESTED_PASSWORD);
+
+    expect(result).toBe(EXPECTED_VALUE);
   });
 });
